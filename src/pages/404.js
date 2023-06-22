0a48cf34349cc4503eb728f6c699e5ff27696d92
zamroni <forheron@gmail.com>
2023-06-23 01:27:29 +0700

    add funny page

---
+++ src/pages/404.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Empat = () => {
+  return (
+    <div>404</div>
+  )
+}
---
+++ src/pages/jokes/index.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Jokes = () => {
+  return (
+    <div>index</div>
+  )
+}
